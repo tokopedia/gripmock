13e82e694e0d3d42ea9d0ec814e9c45265311d0b
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2019-09-16 18:46:35 +0700

    run simple example

---
+++ example/simple/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/tokopedia/gripmock/example/simple"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// Set up a connection to the server.
+	conn, err := grpc.DialContext(ctx, "localhost:4770", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewSimpleClient(conn)
+
+	// Contact the server and print out its response.
+	name := "tokopedia"
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.SayHello(context.Background(), &pb.Request{Name: name})
+	if err != nil {
+		log.Fatalf("error from grpc: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
--- test/example/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-gripmock --stub=example/stubs example/pb/hello.proto &
-
-sleep 5
-
-go run example/client/go/*.go
