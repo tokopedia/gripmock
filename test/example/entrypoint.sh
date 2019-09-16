b1d1d04d48d7de2539e821d7a9fb980458312d99
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2019-09-16 17:26:52 +0700

    only run

--- test/example/entrypoint.sh
+++ test/example/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-gripmock --grpc-listen='0.0.0.0' --stub=example/stubs example/pb/hello.proto &
+gripmock --grpc-listen='0.0.0.0' --stub=example/stubs example/pb/hello.proto
 
-go run example/client/go/*.go
+#go run example/client/go/*.go
