f1c747efd4593c494650898b430cfe1ef9ad3815
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2021-08-02 19:02:02 +0700

    cat and ls

--- example/simple/entrypoint.sh
+++ example/simple/entrypoint.sh
@@ -4,9 +4,11 @@
 
 gripmock --stub=${GRIPMOCK_DIR}example/simple/stub ${GRIPMOCK_DIR}example/simple/simple.proto &
 
-pwd
-
 # wait for generated files to be available and gripmock is up
 sleep 2
+
+cat /go/src/grpc/server.go
+
+ls ${GRIPMOCK_DIR}example/simple
 
 go run ${GRIPMOCK_DIR}example/simple/client/*.go
