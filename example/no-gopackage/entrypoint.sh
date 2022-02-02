dfe47068664546f6c5d2f9ce188d1de9e4189b85
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2022-02-02 17:39:16 +0700

    fix typo
    fix typo in entrypoint.sh and integration-test.yml

--- example/no-gopackage/entrypoint.sh
+++ example/no-gopackage/entrypoint.sh
@@ -2,13 +2,12 @@
 
 # this file is used by .github/workflows/integration-test.yml
 
-# we need to add example/multi-package ar as included import path
-# without it protoc could not find the bar/bar.proto
-gripmock --stub=example/multi-package/stub --imports=example/multi-package/ \
-  example/multi-package/foo.proto example/multi-package/hello.proto \
-  example/multi-package/bar/bar.proto &
+gripmock --stub=example/no-gopackage/stub \
+  example/no-gopackage/foo.proto example/no-gopackage/hello.proto \
+  example/no-gopackage/bar/bar.proto \
+  example/no-gopackage/bar/deep/bar.proto &
 
 # wait for generated files to be available and gripmock is up
 sleep 2
 
-go run example/multi-package/client/*.go
+go run example/no-gopackage/client/*.go
